8024d4e01112d1f3bb706bae60b708b608b02b9c
Max <dun.max@yandex.ru>
2018-11-27 15:10:39 +0300

    Try

---
+++ src/main/java/JDBC.java
@@ -0,0 +1,33 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static java.sql.DriverManager.*;
+
+
+public class JDBC {
+    String name = "test_user";
+    String password = "qwerty";
+    String url = "jdbc:postgresql://localhost:5432/test_database";
+    Connection conn;
+    try{
+        Class.forName("org.postgresql.Driver");
+    }catch(ClassNotFoundException e){
+        System.err.println("driver not found");
+        e.printStackTrace();
+    }
+    try{
+        conn = DriverManager.getConnection(url,name,password);
+    }
+    catch(SQLException e){
+        System.err.println("Connection failed");
+        e.printStackTrace();
+    }
+    if(conn == null)
+    {
+        System.err.println("Connection failed");
+    }
+    }
+
