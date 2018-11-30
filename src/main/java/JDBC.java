4443def5a25fc0012e8fc1a2cc27b8aed7bda7a3
Max <dun.max@yandex.ru>
2018-11-30 17:48:16 +0300

    Разбирался в JDBC

--- src/main/java/JDBC.java
+++ src/main/java/JDBC.java
@@ -1,30 +1,63 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 
+public class JDBC  {
+    public static void main(String[] args) {
+        String name = "test_user";
+        String password = "qwerty";
+        String url = "jdbc:postgresql://localhost:5432/test_database";
+        Connection conn = null;
+        Statement st  =null;
+        ResultSet rs = null;
+        String query = " ";
+            try {
+                Class.forName("org.postgresql.Driver");
+            } catch (ClassNotFoundException e){
+                System.err.println("driver not found");
+                e.printStackTrace();
+            }
+            try {
+                conn = DriverManager.getConnection(url,name,password);
+                System.out.println("Connection sucsess");
+                st = conn.createStatement();
+                String SQL = "CREATE TABLE if not exists Clients " +
+                        "(id INTEGER not NULL, " +
+                        " name VARCHAR(50), " +
+                        " City VARCHAR (50), " +
+                        " PRIMARY KEY (id))";
+                //query = "insert into Clients (id,NAME, City ) values (1 ,'Ciri Dun', 'Voronezh');";
+                st.executeUpdate(SQL);
+                st.executeUpdate(query);
+                query ="Select * from Clients ; ";
+                rs = st.executeQuery(query);
 
-public class JDBC {
-    String name = "test_user";
-    String password = "qwerty";
-    String url = "jdbc:postgresql://localhost:5432/test_database";
-    Connection conn;
-    try{
-        Class.forName("org.postgresql.Driver");
-    }catch(ClassNotFoundException e){
-        System.err.println("driver not found");
-        e.printStackTrace();
+                while(rs.next())
+                {
+                    int id = rs.getInt(1);
+                    String first_name = rs.getString(2);
+                    String city = rs.getString(3);
+                    System.out.println("result: "+ id +  first_name + city );
+                }
+
+            }
+            catch(SQLException e){
+                System.err.println("failed");
+                e.printStackTrace();
+
+            }
+            finally {
+                try{conn.close();} catch(SQLException se){
+                    System.out.println("can't close connection");se.printStackTrace();}
+                    try{st.close();} catch (SQLException se1){
+                        System.out.println("Can't close statement");se1.printStackTrace();
+                    }
+                    try{rs.close();} catch (SQLException se2) {
+                        System.out.println("Can't close result");se2.printStackTrace();}
+                }
+            }
+
+
     }
-    try{
-        conn = DriverManager.getConnection(url,name,password);
-    }
-    catch(SQLException e){
-        System.err.println("Connection failed");
-        e.printStackTrace();
-    }
-    if(conn == null)
-    {
-        System.err.println("Connection failed");
-    }
-    }
+
+
 
