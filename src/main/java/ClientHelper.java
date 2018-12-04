476c49343f0e909dac5c63156419837995b1f86c
Max <dun.max@yandex.ru>
2018-12-04 15:43:27 +0300

    Добавил некоторый функционал

--- src/main/java/ClientHelper.java
+++ src/main/java/ClientHelper.java
@@ -11,34 +11,30 @@ public  class  ClientHelper {
 
     public ClientHelper(Linker l) {
         link = l;
+        try {
+            st = link.getConnection().createStatement();
+        }catch(SQLException e){
+            System.err.println("Statement failed"); e.printStackTrace();
+        }
     }
 
 
     public <T> ResultSet  SelectClient(T option) {
         ResultSet rs = null;
         String query = null;
-        try {
-            st = link.getConnection().createStatement();
-        }catch(SQLException e){
-            System.err.println("Statement failed"); e.printStackTrace();
-        }
+
         if(option instanceof Integer)
-             query = "Select from clients where id = " + option + " ;";
+             query = "Select * from clients where id = '" + option + "';";
         else if(option instanceof String && ((String) option).split(" ").length > 1)
-             query = "Select from clients where name is like " + option + ";";
-        else if(option instanceof String) query = "Select from clients where city is like " + option + "; ";
+             query = "Select * from clients where name = '" + option + "';";
+        else if(option instanceof String) query = "Select * from clients where city = '" + option + "' ; ";
         try {
              rs = st.executeQuery(query);
         }catch(SQLException e){
             System.err.println("Invalid result");e.printStackTrace();
         }
-        finally {
-            try{rs.close();} catch (SQLException e) {
-                 System.out.println("Can't close result");e.printStackTrace();}
-            try {st.close();} catch (SQLException e){
-                System.err.println("Failed to close statement"); e.printStackTrace();
-            }
-        }
+
+
         return rs;
     }
     public void PrintResult(ResultSet rs) {
@@ -48,7 +44,7 @@ public  class  ClientHelper {
                     int id = rs.getInt(1);
                     String first_name = rs.getString(2);
                     String city = rs.getString(3);
-                    System.out.println("result: "+ id +  first_name + city );
+                    System.out.println("result: "+ id +" " +  first_name+" " + city );
                 }
         }catch(SQLException e){
             System.err.println("Failed to print");e.printStackTrace();
@@ -63,10 +59,6 @@ public  class  ClientHelper {
 
 
     public void DeleteClient(int option){
-        try {
-            st = link.getConnection().createStatement();
-        }catch (SQLException e){
-            System.err.println("st fail"); e.printStackTrace();}
             String query = "Delete from clients where id = " + option + " ;" ;
         try {
             st.executeUpdate(query);
@@ -77,12 +69,6 @@ public  class  ClientHelper {
     }
 
     public void AddClient(int id, String Name, String City) {
-        try {
-             st = link.getConnection().createStatement();
-        } catch (SQLException e) {
-            System.err.println("Statement not created");
-            e.printStackTrace();
-        }
         try {String query = "INSERT INTO CLIENTS (id,Name,City) VALUES ("+
                 id +", '"+ Name+ "' , '" + City + "');"; st.executeUpdate(query);}
                 catch (SQLException e)
--- src/main/java/JDBC.java
+++ src/main/java/JDBC.java
@@ -1,46 +1,47 @@
 import java.sql.*;
 
 
-public class JDBC  {
+public class JDBC {
     public static void main(String[] args) {
         String name = "test_user";
         String password = "qwerty";
         String url = "jdbc:postgresql://localhost:5432/test_database";
         // объект, соединяющий БД с джава
-        Linker l = new Linker(url,name,password);
+        Linker l = new Linker(url, name, password);
 
         ResultSet rs = null;
-        String query = " ";
-            try {
-                l.connect();
-                System.out.println("Connection sucsess");
-                ClientHelper helper = new ClientHelper(l); // Объект, позволяющий взаимподействовать с таблицей Clients
-                String SQL = "CREATE TABLE if not exists Clients " +
+        l.connect();
+        System.out.println("Connection sucsess");
+        ClientHelper helper = new ClientHelper(l); // Объект, позволяющий взаимподействовать с таблицей Clients
+        rs = helper.SelectClient("Voronezh");
+        helper.PrintResult(rs);
+    }
+}
+
+               /* String SQL = "CREATE TABLE if not exists Clients " +
                         "(id INTEGER not NULL, " +
                         " name VARCHAR(50), " +
                         " City VARCHAR (50), " +
                         " PRIMARY KEY (id))";
                 //query = "insert into Clients (id,NAME, City ) values (1 ,'Ciri Dun', 'Voronezh');";
                 //st.executeUpdate(SQL);
-                helper.DeleteClient(3);
-                query ="Select * from Clients ; ";
-                //rs = st.executeQuery(query);
 
 
 
-            }
 
-
-            finally {
+            }  finally {
                 try{l.getConnection().close();} catch(SQLException se){
                     System.out.println("can't close connection");se.printStackTrace();}
-                   // try{rs.close();} catch (SQLException se2) {
-                       // System.out.println("Can't close result");se2.printStackTrace();}
-                }
-            }
+                // try{rs.close();} catch (SQLException se2) {
+                // System.out.println("Can't close result");se2.printStackTrace();}
+            }*/
 
 
-    }
+
+
+
+
+
 
 
 
